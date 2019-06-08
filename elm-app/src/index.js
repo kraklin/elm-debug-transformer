48999d511072d7e86f191780f293edfdde9d1687
Tomas Latal <latal.tomas@gmail.com>
2019-06-08 15:50:15 +0200

    Prettier format

--- elm-app/src/index.js
+++ elm-app/src/index.js
@@ -3,13 +3,12 @@ import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 import * as ElmDebug from '../../src/index.js';
 
-
 ElmDebug.register();
 
 Elm.Main.init({
   node: document.getElementById('root')
 });
 
-console.log("some value");
+console.log('some value');
 
 registerServiceWorker();
--- src/index.js
+++ src/index.js
@@ -1,58 +1,74 @@
 import * as Parser from './parser.js';
-import * as _ from 'lodash'
+import * as _ from 'lodash';
 
-export function register () {
+export function register() {
   const _log = console.log;
 
-  window.devtoolsFormatters = [{
-    header: function(obj, config){
-        if ((!!obj.type && obj.type === 'ElmDebug')||(!!config && config.elmFormat)){
-          const prefix = (!!config && !!config.key) ? [["span", {style: "color: purple; font-weight: bold"}, config.key], ["span", {}, " = "]] : [];
-
-          return ["div", {}, "(h)"].concat(prefix).concat(handleHeader(obj,config))
+  window.devtoolsFormatters = [
+    {
+      header: function(obj, config) {
+        if (
+          (!!obj.type && obj.type === 'ElmDebug') ||
+          (!!config && config.elmFormat)
+        ) {
+          if (!!config && !!config.key) {
+            return keyValueLine(config.key, handleHeader(obj, config));
+          } else {
+            return ['div', {}, '(h)', handleHeader(obj, config)];
+          }
+        } else {
+          return null;
         }
-        else { return null; }
-    },
-    hasBody: function(obj){
+      },
+      hasBody: function(obj) {
         return true;
-    },
-    body: function(obj, config){
-        return ["div", {}, "(b)", handleBody(obj, config)]
+      },
+      body: function(obj, config) {
+        return ['div', {}, '(b)', handleBody(obj, config)];
+      }
     }
-  }]
+  ];
 
-  console.log = ((msg) => {
+  console.log = msg => {
     try {
       const parsed = Parser.parse(msg);
       _log.call(console, JSON.parse(JSON.stringify(parsed)));
       // for debug only
       _log.call(console, parsed.value);
-
-  } catch (err) {
+    } catch (err) {
       _log.call(console, msg);
     }
-  });
+  };
 }
 
+function keyValueLine(key, value) {
+  return [
+    'div',
+    {},
+    ['span', { style: 'color: purple; font-weight: bold' }, key],
+    ['span', {}, ' = '], value 
+  ];
+}
 
 function handleHeader(value, config) {
   if (!value.type || !value.value) {
     return null;
   }
 
-
-  switch(value.type){
+  switch (value.type) {
     case 'ElmDebug':
-      const tag = (!!value.tag) ? value.tag + ": " : "";
+      const tag = !!value.tag ? value.tag + ': ' : '';
 
       return tag + handleHeader(value.value, config);
     case 'Record':
       const keys = _.chain(value.value)
-        .mapValues((v, k)=> {return k+" = "+ handleHeader(v, config)})
+        .mapValues((v, k) => {
+          return k + ' = ' + handleHeader(v, config);
+        })
         .values()
         .value();
 
-      return `{ ${_.truncate(keys.join(", "))} }`;
+      return `{ ${_.truncate(keys.join(', '))} }`;
 
     case 'String':
       return `"${value.value}"`;
@@ -66,26 +82,34 @@ function handleBody(value, config) {
   //const value = (!!config)?config.value: obj;
 
   if (!value.type || !value.value) {
-      return ["div", {}, "end"];
+    return ['div', {}, 'end'];
   }
 
-  switch(value.type){
+  switch (value.type) {
     case 'ElmDebug':
-      return ["div", {}, handleBody(value.value,{})];
+      return ['div', {}, handleBody(value.value, {})];
     case 'Record':
       const values = _.chain(value.value)
-        .mapValues((v, k)=> {if(_.isString(v)) { return ["span",{style: "color:red"},k+"=" + v] } return ["div", {}, ["object", {object: v, config: {key: k, elmFormat: true}}]]})
+        .mapValues((v, k) => {
+          if (_.isString(v)) {
+            return keyValueLine(k, `"${v}"`);
+          }
+          return [
+            'div',
+            {},
+            ['object', { object: v, config: { key: k, elmFormat: true } }]
+          ];
+        })
         .values()
         .value();
 
-      return ["div", {}].concat(values);
+      return ['div', {}].concat(values);
     case 'String':
       return null; //value.value;
 
     default:
-      return ["div", {}, JSON.stringify({o: {}, v: value})];
+      return ['div', {}, JSON.stringify({ o: {}, v: value })];
   }
 
-  return ["div", {}, "body"]
+  return ['div', {}, 'body'];
 }
-
