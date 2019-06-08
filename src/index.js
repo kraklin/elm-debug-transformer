4a8df63272c979a2fe61ca87d6288816c94bf0d8
Tomas Latal <latal.tomas@gmail.com>
2019-06-08 15:58:42 +0200

    Recognise numbers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,86 @@
 import * as Parser from './parser.js';
 import * as _ from 'lodash';
 
+function keyValueLine(key, value) {
+  return [
+    'div',
+    {},
+    ['span', { style: 'color: purple; font-weight: bold' }, key],
+    ['span', {}, ' = '], value 
+  ];
+}
+
+function isFinalValue(value) {
+  return (_.isString(value) || (value.type == "Number"))
+}
+
+function getFinalValue(value) {
+  if (_.isString(value) )
+    return `"${value}"`;
+  else
+    return value.value
+}
+
+function handleHeader(value, config) {
+  if (!value.type || !value.value) {
+    return null;
+  }
+
+  switch (value.type) {
+    case 'ElmDebug':
+      const tag = !!value.tag ? value.tag + ': ' : '';
+
+      return tag + handleHeader(value.value, config);
+    case 'Record':
+      const keys = _.chain(value.value)
+        .mapValues((v, k) => {
+          return k + ' = ' + handleHeader(v, config);
+        })
+        .values()
+        .value();
+
+      return `{ ${_.truncate(keys.join(', '))} }`;
+
+    case 'String':
+      return `"${value.value}"`;
+
+    default:
+      return toString(value);
+  }
+}
+
+function handleBody(value, config) {
+  if (!value.type || !value.value) {
+    return ['div', {}, 'end'];
+  }
+
+  switch (value.type) {
+    case 'ElmDebug':
+      return ['div', {}, handleBody(value.value, {})];
+    case 'Record':
+      const values = _.chain(value.value)
+        .mapValues((v, k) => {
+          if (isFinalValue(v)) {
+            return keyValueLine(k, getFinalValue(v));
+          }
+          return [
+            'div',
+            {},
+            ['object', { object: v, config: { key: k, elmFormat: true } }]
+          ];
+        })
+        .values()
+        .value();
+
+      return ['div', {}].concat(values);
+
+    default:
+      return ['div', {}, JSON.stringify({ o: {}, v: value })];
+  }
+
+  return ['div', {}, 'body'];
+}
+
 export function register() {
   const _log = console.log;
 
@@ -41,75 +121,3 @@ export function register() {
   };
 }
 
-function keyValueLine(key, value) {
-  return [
-    'div',
-    {},
-    ['span', { style: 'color: purple; font-weight: bold' }, key],
-    ['span', {}, ' = '], value 
-  ];
-}
-
-function handleHeader(value, config) {
-  if (!value.type || !value.value) {
-    return null;
-  }
-
-  switch (value.type) {
-    case 'ElmDebug':
-      const tag = !!value.tag ? value.tag + ': ' : '';
-
-      return tag + handleHeader(value.value, config);
-    case 'Record':
-      const keys = _.chain(value.value)
-        .mapValues((v, k) => {
-          return k + ' = ' + handleHeader(v, config);
-        })
-        .values()
-        .value();
-
-      return `{ ${_.truncate(keys.join(', '))} }`;
-
-    case 'String':
-      return `"${value.value}"`;
-
-    default:
-      return toString(value);
-  }
-}
-
-function handleBody(value, config) {
-  //const value = (!!config)?config.value: obj;
-
-  if (!value.type || !value.value) {
-    return ['div', {}, 'end'];
-  }
-
-  switch (value.type) {
-    case 'ElmDebug':
-      return ['div', {}, handleBody(value.value, {})];
-    case 'Record':
-      const values = _.chain(value.value)
-        .mapValues((v, k) => {
-          if (_.isString(v)) {
-            return keyValueLine(k, `"${v}"`);
-          }
-          return [
-            'div',
-            {},
-            ['object', { object: v, config: { key: k, elmFormat: true } }]
-          ];
-        })
-        .values()
-        .value();
-
-      return ['div', {}].concat(values);
-    case 'String':
-      return null; //value.value;
-
-    default:
-      return ['div', {}, JSON.stringify({ o: {}, v: value })];
-  }
-
-  return ['div', {}, 'body'];
-}
