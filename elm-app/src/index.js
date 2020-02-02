86fdfa0863ea1a18cf2f678c2c51592042456819
Tomas Latal <latal.tomas@gmail.com>
2020-02-02 17:57:03 +0100

    Simplify debug elm app

--- elm-app/src/index.js
+++ elm-app/src/index.js
@@ -1,16 +1,10 @@
-import './main.css';
 import { Elm } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 import { register } from '../../dist/elm-console-debug.js';
 
 let opts = {simple_mode: false, debug: true, sizeLimit: 1000}
 
 window.elmopts = register(opts);
-window.elmopts2 = register(opts);
-window.elmopts3 = register(opts);
 
 Elm.Main.init({
     node: document.getElementById('root'),
 });
-
-registerServiceWorker();
