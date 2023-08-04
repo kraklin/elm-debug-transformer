78e539fb32c47df55579baf201f67130e7977ea6
Tomáš  Látal <latal.tomas@gmail.com>
2023-08-05 01:38:04 +0200

    fix exported functions

--- elm-app/src/index.js
+++ elm-app/src/index.js
@@ -1,6 +1,6 @@
 import { Elm } from './Main.elm';
-import * as ElmDebugger from '../../dist/index.mjs';
-//import * as ElmDebugger from 'elm-debug-transformer';
+//import * as ElmDebugger from '../../dist/index.mjs';
+import * as ElmDebugger from 'elm-debug-transformer';
 
 console.log(ElmDebugger);
 ElmDebugger.register({simple_mode: false, debug: true, limit: 10000});
